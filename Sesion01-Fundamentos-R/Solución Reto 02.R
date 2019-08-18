40e4310c83bedf908d0fef160d70b5399fb5f343
arizasilvia8 <53496518+arizasilvia8@users.noreply.github.com>
2019-08-18 18:56:56 -0500

    Add files via upload

---
+++ Sesion01-Fundamentos-R/Solución Reto 02.R
@@ -0,0 +1,8 @@
+# Ejercicio 02: 
+# De todos los paquetes existentes, instala uno, que ya quedará guardado en la memoria de tu R Studio para siempre! 
+# A continuación, apóyate del módulo de ayuda para saber más sobre su funcionalidad. 
+install.packages("dplyr")
+
+library("ggplot2")
+
+?dplyr
