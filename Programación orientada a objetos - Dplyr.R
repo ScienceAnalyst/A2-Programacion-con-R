55d21bf8f4b6609abba379c81ed5e15254f34104
arizasilvia8 <53496518+arizasilvia8@users.noreply.github.com>
2019-08-19 11:51:44 -0500

    Update Programación orientada a objetos - Dplyr.R

--- Programación orientada a objetos - Dplyr.R
+++ Programación orientada a objetos - Dplyr.R
@@ -1,73 +1,73 @@
 
-## Introducci??n al package dplyr 
+## Introducción al package dplyr 
 install.packages("dplyr")
 library(dplyr)
 
-# Dplyr nos ayudar?? a hacer consultas, agregaciones y filtros de datos de manera r??pida
+# Dplyr nos ayudar?? a hacer consultas, agregaciones y filtros de datos de manera rápida
 # Su uso es similar al de SQL (dispone de funciones como select, left join, inner join y group by)
-# Adem??s, dispone de algunas propias (filter, summarise, arrange y mutate)
+# Además, dispone de algunas propias (filter, summarise, arrange y mutate)
 
-# Importamos el fichero llamado data para poder operar con ??l a continuaci??n 
+# Importamos el fichero llamado data para poder operar con él a continuación 
 datos <- read.csv("data.csv", header=TRUE)
 head(data) # Para visualizar los primeros registros 
 
-# Muestra aleatoria de la base. El segundo componente indica el n??mero de filas que queremos
-# Atenci??n! Esta muestra aleatoria cambiar?? cada vez que ejecutemos el c??digo, por lo que los resultados
-# del an??lisis variar??n 
+# Muestra aleatoria de la base. El segundo componente indica el número de filas que queremos
+# Atención! Esta muestra aleatoria cambiará cada vez que ejecutemos el código, por lo que los resultados
+# del análisis variarín 
 sample_n(datos,8)
 
-# Tambi??n podemos importar informaci??n de manera aleatoria pero indicando el porcentaje que deseamos
+# También podemos importar información de manera aleatoria pero indicando el porcentaje que deseamos
 sample_frac(datos,0.1) # 10%
 
 # Si dudamos de la existencia de duplicados, podemos usar la funci??n distinct
 distinct(datos)
 
-# Para seleccionar informaci??n espec??fica de la tabla 
+# Para seleccionar información específica de la tabla 
 select(datos, Indice, Estado:A2008)
 
-# Si nos es m??s f??cil indicar que queremos llevar todas las variables excepto alguna/s, lo indicaremos 
-# con el s??mbolo "-" delante de la variable
-select(datos, -Indice, -Estado) # ent este caso, solo el ??ndice y el Estado ser??n omitidos 
+# Si nos es más fácil indicar que queremos llevar todas las variables excepto alguna/s, lo indicaremos 
+# con el símbolo "-" delante de la variable
+select(datos, -Indice, -Estado) # ent este caso, solo el Índice y el Estado serín omitidos 
 
-# Tambi??n podemos filtrar seg??n el primer car??cter 
-select(datos, starts_with("A")) # Importar?? toda la informaci??n que contienen los a??os 
-select(mydata, -starts_with("A")) # Esta funci??n traer?? la informaci??n complementaria al comando anterior
+# También podemos filtrar según el primer carácter 
+select(datos, starts_with("A")) # Importará toda la información que contienen los años 
+select(mydata, -starts_with("A")) # Esta función traerá la información complementaria al comando anterior
 
-# Una de las maneras para cambiar el nombre de las columnas es la funci??n rename
+# Una de las maneras para cambiar el nombre de las columnas es la función rename
 rename(datos, Indice1 = Indice)
-datos <- rename(datos, Indice1 = Indice) # As?? logramos que los cambios se guarden 
+datos <- rename(datos, Indice1 = Indice) # Así logramos que los cambios se guarden 
 
-# La funci??n filter facilita la filtraci??n de informaci??n 
-filter(datos, Indice1 == "A") # Informaci??n de aquellos Estados que su ??ndice es "A"
+# La función filter facilita la filtración de información 
+filter(datos, Indice1 == "A") # Información de aquellos Estados que su índice es "A"
 
-# M??ltiples criterios de selecci??n
+# M??ltiples criterios de selección
 filter(datos, Indice1 %in% c("A", "B")) 
 
 # Filtrar por varias variables separando los comandos por "&" 
 filter(datos, Indice1 %in% c("A", "B") & A2008 >= 1300000 )
 
-# Para indicar condiciones disyuntivas (equivalente a "or" en SQL, ser??a "|" ) 
+# Para indicar condiciones disyuntivas (equivalente a "or" en SQL, sería "|" ) 
 # Solo tiene que cumplir una de ambas condiciones para ser fitrado 
 filter(datos, Indice1 %in% c("A", "B") | A2008 >= 1300000)
 
-# Al igual que SQL, el s??mbolo "!" indica que no est??/cumpla una condici??n
+# Al igual que SQL, el símbolo "!" indica que no cumpla una condición
 filter(datos, !Indice1 %in% c("A", "B"))
 
-# La funci??n grepl ayuda a extraer informaci??n de un campo en particular 
-filter(datos, grepl("kan", Estado)) # Informaci??n de aquellos estados que contengan "kan" 
+# La función grepl ayuda a extraer información de un campo en particular 
+filter(datos, grepl("kan", Estado)) # Información de aquellos estados que contengan "kan" 
 
-# Summarise es una funci??n muy ??til para hacer operaciones r??pidas sobre variables
+# Summarise es una función muy ??til para hacer operaciones rápidas sobre variables
 summarise(datos, A2012_mean = mean(A2012)) # Media de todos los estados en 2012 
 
-# Summarise_at nos permite escribir varias condiciones de agrupaci??n de las operaciones 
+# Summarise_at nos permite escribir varias condiciones de agrupación de las operaciones 
 summarise_at(datos, vars(A2012, A2013), funs(n(), mean, median))
 
-# Funci??n arrange para ordenar informaci??n seg??n varias variables
+# Funci??n arrange para ordenar información según varias variables
 arrange(datos, Indice1, A2012) # Por defecto, esta ordenaci??n es ascendente 
 
 # Otra manera de separar varias funciones es usar "%>%"
 datos %>%
   group_by(Indice1)%>%
-  summarise(Media2012 = mean(A2012, na.rm=TRUE), #NA remove para evitar tener registros vac??os que sesguen la informaci??n 
+  summarise(Media2012 = mean(A2012, na.rm=TRUE), #NA remove para evitar tener registros vacíos que sesguen la información 
             Media2014 = mean(A2014, na.rm=TRUE)) %>%
-  arrange(desc(Mean_2015)) 
+  arrange(desc(Media2012)) 
