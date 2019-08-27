f0947af4317746eae4ca3980522587cf040b42a9
wicho1001 <wichoguerrero@yahoo.com>
2019-08-27 14:10:56 -0500

    feat: Agregado estructura de carpetas

---
+++ Sesion01-Fundamentos-R/Ejemplo-01/Readme.md
@@ -0,0 +1,10 @@
+# Ejercicio 01: 
+  
+#Vamos a realizar una operación, usaremos R como calculadora. 
+#¿Cuantas horas tiene un año de 365 días? 
+
+```{r}
+365*12*28*24*60*60
+```
+
+# Solución: 10,596,096,000
--- Sesion01-Fundamentos-R/Solución Reto 02.R
+++
@@ -1,8 +0,0 @@
-# Ejercicio 02: 
-# De todos los paquetes existentes, instala uno, que ya quedará guardado en la memoria de tu R Studio para siempre! 
-# A continuación, apóyate del módulo de ayuda para saber más sobre su funcionalidad. 
-install.packages("dplyr")
-
-library("ggplot2")
-
-?dplyr
