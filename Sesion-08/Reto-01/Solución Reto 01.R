65210f521a87c60a590952daf3b00c471bdb5803
wicho1001 <wichoguerrero@yahoo.com>
2019-09-25 12:26:10 -0500

    feat: Removiendo soluciones de los retos de la sesión 8

--- Sesion-08/Reto-01/Solución Reto 01.R
+++
@@ -1,7 +0,0 @@
-head(mtcars)
-scatter.smooth(x=mtcars$wt, y=mtcars$mpg, main="Peso y Millas por Galon")  
-cor(mtcars$wt, mtcars$mpg) 
-modlin <- lm(wt ~ mpg, data=mtcars)  
-print(modlin)
-summary(modlin)
-
