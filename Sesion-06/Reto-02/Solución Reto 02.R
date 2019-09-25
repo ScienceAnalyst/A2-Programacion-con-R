35b92862b9bb6040d8bc0946d7864baff7144f3f
wicho1001 <wichoguerrero@yahoo.com>
2019-09-25 12:26:01 -0500

    feat: Removiendo soluciones de los retos de la sesión 6

--- Sesion-06/Reto-02/Solución Reto 02.R
+++
@@ -1,7 +0,0 @@
-
-leche_mes$ano<-format(leche_mes$month, format = "%Y")
-leche_mes$mes_num <- format(leche_mes$month, format = "%m")
-
-ggplot(leche_mes, aes(x = mes_num, y = milk_prod_per_cow_kg, group = ano)) + 
-  geom_line(aes(colour = ano)) + 
-  theme_classic() 
