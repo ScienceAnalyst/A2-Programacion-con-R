d42ede711b315e38fa005e8458fe0be2c09470e1
Rodmck13 <45569705+Rodmck13@users.noreply.github.com>
2020-01-08 17:04:43 -0600

    Actualizando Solución Reto 2

--- Sesion-03/Reto-02/Solución Reto 02.R
+++ Sesion-03/Reto-02/Solución Reto 02.R
@@ -1,8 +1,36 @@
+### Utilizando el Dataset de airquality.
 
-library("readxl")
-ecobici <- read_excel("ecobici.xls")
-head(ecobici)
+# Investigar el detalle de cada columna del DataSet.
 
-for(i in ecobici$latitud>19.423396){
-  print(i)
-}
+data("airquality")
+??airquality
+
+# Utilizando ciclo For y la sentencia IF/ELSE; 
+    # Contar cuantos días el promedio de velocidad del aire es: Mayor a 7, Menor a 4 y 
+    # cuántos registros existen dentro de ese rango(4-7).
+      
+      menor <- 0
+      dentro <- 0
+      mayor <- 0
+      for(i in airquality$Wind){
+        if(i > 7){
+          mayor <- mayor + 1
+        }else{
+          if(i < 4){
+            menor <- menor + 1
+          }else{
+            dentro <- dentro + 1
+          }
+        }
+      }
+     print(mayor)
+     print(menor)
+     print(dentro)
+    
+# Calcular el promedio total de la Columna Temp.
+
+      total <- 0
+      for(i in airquality$Temp){
+        total <- (total + i)
+      }
+      print(total/(nrow(airquality)))
