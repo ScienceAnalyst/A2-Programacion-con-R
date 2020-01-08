243fdae375c26fce898796831b8f78b7bd6ce098
Rodmck13 <45569705+Rodmck13@users.noreply.github.com>
2020-01-08 11:43:31 -0600

    #Condición con OR o AND

--- Sesion-03/Ejemplo-03/Ejemplo-03.R
+++ Sesion-03/Ejemplo-03/Ejemplo-03.R
@@ -1,6 +1,24 @@
-precio <- 20
+#Condición simple.
 
-while(precio <= 2500){
-  print(precio)
-  precio <- precio + 5 
+variable <- 8
+if (variable > 40){
+print("La variable es superior a 40")
+} else {
+print ("La variable no es superior a 40")
 }
+
+ifelse(variable>40, "La variable es superior a 40", "La variable no es superior a 40")
+
+#Condición con OR o AND
+
+A <- 8
+B <- 17
+C <- 10
+D <- 10
+if (A > B || C == D){ # probar con AND (&)
+print("TRUE")
+} else {
+print ("FALSE")
+}
+
+ifelse(A > B || C == D, "TRUE", "FALSE")
