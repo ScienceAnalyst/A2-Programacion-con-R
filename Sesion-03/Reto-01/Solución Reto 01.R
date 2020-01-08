167b2c0a1f76542185412c2a20516707cf338a60
Rodmck13 <45569705+Rodmck13@users.noreply.github.com>
2020-01-08 15:52:26 -0600

    Modificando Reto1 Solución..

--- Sesion-03/Reto-01/Solución Reto 01.R
+++ Sesion-03/Reto-01/Solución Reto 01.R
@@ -1,22 +1,14 @@
+## Crear una función que reciba un array de numeros positivos y utilizando if/else 
+## determine cual es el valor mayor en el array
 
-library("readxl")
-ecobici <- read_excel("ecobici.xls")
-head(ecobici)
+mayor <- function(array){
+  numero <- 0
+  for(i in array){
+    if(i >  numero){
+      numero <- i;
+    }
+  }
+  print(numero)
+}
 
-sum(is.na(ecobici$nearbyStations))
-ecobici$estacion1 <- ifelse(ecobici$nearbyStations>=1, 1, 0)
-
-sum(is.na(ecobici$nearbyStations2))
-ecobici$estacion2 <- ifelse(ecobici$nearbyStations2>=1, 1, 0)
-
-sum(is.na(ecobici$nearbyStations3))
-ecobici$estacion3 <- ifelse(ecobici$nearbyStations3>=1, 1, 0)
-
-sum(is.na(ecobici$nearbyStations4))
-ecobici$estacion4 <- ifelse(ecobici$nearbyStations4>=1, 1, 0)
-
-sum(is.na(ecobici$nearbyStations5))
-ecobici$estacion5 <- ifelse(ecobici$nearbyStations5>=1, 1, 0)
-
-sum(is.na(ecobici$nearbyStations6))
-ecobici$estacion6 <- ifelse(ecobici$nearbyStations6>=1, 1, 0)
+mayor(c(10, 12, 2, 394, 244,23))
