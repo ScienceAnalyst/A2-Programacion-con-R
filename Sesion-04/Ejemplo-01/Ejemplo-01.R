cd1c2c00f96a5af5839ab56e7c386e8d69a9ce90
Rodmck13 <45569705+Rodmck13@users.noreply.github.com>
2020-01-10 12:33:02 -0600

    Modificando Ejemplo 1.

--- Sesion-04/Ejemplo-01/Ejemplo-01.R
+++ Sesion-04/Ejemplo-01/Ejemplo-01.R
@@ -1,5 +1,14 @@
-library(dplyr)
-mtcars <- mtcars 
-sum(is.na(mtcars))
-str(mtcars)
-cor(mtcars$hp,mtcars$mpg)
+head(spotify) #Conocer como está estructurado el dataset
+str(spotify)  #Conocer a detalle los datos de cada columna  
+nrow(spotify) #Conocer el número de observaciones.
+ncol(spotify) #Conocer el número de columnas.
+mean(spotify$duration_ms) #Conocer la media de duración de las canciones en ms.
+median(spotify$acousticness)#Conocer la mediana de acústica.
+
+duracion <- ((spotify$duration_ms/1000)/60) #convertir el tiempo en ms a minutos.
+
+#Graficar el historial de casos para conocer en qué rango de minutos existen más registros.
+set.seed(123)
+ejemplo <- rnorm(n = 10000, mean = 0, sd = 1)
+hist(duracion, col='orange', breaks=40, 
+     ylab = "Frecuencia", main = "Histograma ejemplo")
